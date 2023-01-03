66e6888e263d7de8a119db9e9c7f439df5dcbc98
Larissa Ramos <devlarissasr@gmail.com>
2023-01-03 03:15:06 +0100

    feat: prettier config

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    arrowParens: 'always',
+    trailingComma: 'all',
+    endOfLine: 'auto',
+    importOrderSeparation: true,
+    experimentalBabelParserPluginsList: ['classProperties', 'jsx', 'typescript'],
+  };
